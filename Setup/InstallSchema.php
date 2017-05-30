925b15eeb135cf41042960915bc1dd3f15fb8812
Kamil Balwierz <kamil@snow.dog>
2017-05-30 15:07:05 +0200

    Install schema file

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -2,6 +2,7 @@
 
 namespace MMAcademy\Greetings\Setup;
 
+use Magento\Framework\DB\Ddl\Table;
 use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
@@ -15,9 +16,30 @@ class InstallSchema implements InstallSchemaInterface
      * @param SchemaSetupInterface   $setup
      * @param ModuleContextInterface $context
      * @return void
+     * @throws \Zend_Db_Exception
      */
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        // TODO: Implement install() method.
+        $installer = $setup;
+
+        $installer->startSetup();
+
+        $tableName = $installer->getTable('mmacademy_message');
+        $table = $installer->getConnection()->newTable($tableName)->addColumn(
+            'message_id',
+            Table::TYPE_INTEGER,
+            null,
+            ['identity' => true, 'nullable' => false, 'primary' => true],
+            'Message ID'
+        )->addColumn('author', Table::TYPE_TEXT, 255, [], 'Author')->addColumn(
+            'value',
+            Table::TYPE_TEXT,
+            '2M',
+            [],
+            'Message'
+        )->addColumn('creation_time', Table::TYPE_DATETIME, null, [], 'Created At');
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
     }
 }
